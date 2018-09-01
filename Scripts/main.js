73db0d97ed34349af59f75f58de04c210add3277
Ross Wilkinson <35375907+rwilkinson93@users.noreply.github.com>
2018-09-01 13:40:34 +0100

    Screeps nooby guide update for prototype spawning

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,5 @@
 // import modules
-var creepCreation = require('mod.spawner');
+require('prototype.spawner')();
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -32,16 +32,17 @@ module.exports.loop = function () {
         else if (creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
+        // if creep is repairer, call repairer script
         else if (creep.memory.role == 'repairer') {
             roleRepairer.run(creep);
         }
     }
 
     //goal: have atleast 6 creeps at all times
-    var minimumNumberOfTotalCreeps = 5;
+    var minimumNumberOfTotalCreeps = 3;
     // goal: have 10 harvesters and as many upgraders as possible
     var minimumNumberOfHarvesters = 6;
-    var minimumNumberOfBuilders = 3;
+    var minimumNumberOfBuilders = 2;
     var minimumNumberOfUpgraders = 1;
     var minimumNumberOfRepairers = ((minimumNumberOfBuilders*2)-1);
     // _.sum will count the number of properties in Game.creeps filtered by the
@@ -51,9 +52,53 @@ module.exports.loop = function () {
     var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
     var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
     var numberOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
+
+    // gets the available energy in the room where Mainbase is
+    var energyAvailable = game.spawn.Mainbase.room.energyAvailable;
+    // gets the total energy capacity in the room where Mainbase is
+    var energyCap = game.spawn.Mainbase.room.energyCapacityAvailable
     var name = undefined;
 
-    creepCreation.run(spawn);
+    // if not enough creeps
+     if (numberOfCreeps < minimumNumberOfTotalCreeps) {
+       //create the smallest possible harvester
+       name = Game.spawns.Mainbase.createCreep([WORK,CARRY,MOVE], undefined
+            { role: 'harvester', working: false});
+     }
+    // if not enough harvesters
+    else if (numberOfHarvesters < minimumNumberOfHarvesters) {
+        // try to spawn one
+        name = Game.spawns.Mainbase.createCustomCreep(energy, 'harvester');
+        // if spawning failed and we have no harvesters left
+        if (name == ERR_NOT_ENOUGH_ENERGY && numberOfHarvesters == 0) {
+            // spawn one with what is available
+            name = Game.spawns.Mainbase.createCustomCreep(
+                Game.spawns.Mainbase.room.energyAvailable, 'harvester');
+        }
+    }
+    // if not enough upgraders
+    else if (numberOfUpgraders < minimumNumberOfUpgraders) {
+        // try to spawn one
+        name = Game.spawns.Mainbase.createCustomCreep(energy, 'upgrader');
+    }
+    // if not enough repairers
+    else if (numberOfRepairers < minimumNumberOfRepairers) {
+        // try to spawn one
+        name = Game.spawns.Mainbase.createCustomCreep(energy, 'repairer');
+    }
+    // if not enough builders
+    else if (numberOfBuilders < minimumNumberOfBuilders) {
+        // try to spawn one
+        name = Game.spawns.Mainbase.createCustomCreep(energy, 'builder');
+    }
+    else {
+        // else try to spawn a upgrader
+        name = Game.spawns.Mainbase.createCustomCreep(energy, 'upgrader');
+    }
+
+    if (!(name < 0)) {
+      console.console.log("Spawned new creep: " + name);
+    }
 
     //prints the total energy available to the console
     console.log("Total energy: " + Game.spawns.Mainbase.room.energyAvailable);
